6bd4f2d7103be99ad095d984c0e6e642a6ad3d88
Deepanshu Sharma <123392107+Deepanshu121020@users.noreply.github.com>
2023-05-28 03:07:26 +0530

    Add files via upload
    Building a Sky Scrapper construction is in such a way that the floors are constructed in other factories and they are assembled. All the sizes are distinct.
    The skyscraper needs to be constructed in N days with the following conditions :
    
    a)	Every day a floor is constructed in a separate factory of distinct size.
    b)	The floor with the larger size must be placed at the bottom of the building.
    c)	The floor with the smaller size must be placed at the top of the building.
    
    Note: A floor cannot be assembled in the building until all floors larger in size are placed.
    
    Built a small program that will help him analyze the construction process, to avoid manual work and errors.
    
    Test Case - TestCase 1
    
    enter the total no of floors in the building
    3
    enter the floor size given on day : 1
    3
    enter the floor size given on day : 2
    1
    enter the floor size given on day : 3
    2
    
    The order of construction is as follows
    
    Day: 1
    3 
    Day: 2
    
    Day: 3
    2 1

---
+++ SkyScrapper/src/com/gl/driver/SkyDriver.java
@@ -0,0 +1,8 @@
+package com.gl.driver;
+
+public class SkyDriver {
+	public static void main(String[] args) {
+		SkyscrapperConstruction skyscrapper = new SkyscrapperConstruction();
+		skyscrapper.input();
+	}
+}
